2e834c59af06271522a14fc8f7204b4d7a8979b4
HoangTuanAnh26 <tuanhoanga789@gmail.com>
2025-02-24 16:35:39 +0900

    Cập nhật phần 3 thực hành về validation form trong khóa học JavaScript cơ bản tại F8

--- JavaScript/Form_validation/validator.js
+++ JavaScript/Form_validation/validator.js
@@ -1,12 +1,19 @@
 //Đối tượng Validator
 function Validator(options) {
     // Lấy element của form cần validate
-    var formElement = document.querySelector(options.form);
+    function getParent(element, selector) {
+        while (element.parentElement) {
+            if (element.parentElement.matches(selector)) {
+                return element.parentElement;
+            }
+            element = element.parentElement;
+        }
+    }
     var selectorRules = {};
 
     // Hàm thực hiện thay đổi thông báo lỗi
     function validate(inputElement, rule) {
-        var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
+        var errorElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector);
         var errorMessage = rule.test(inputElement.value);
 
         // Lấy ra các rules của selector
@@ -16,20 +23,33 @@ function Validator(options) {
         // Nếu có lỗi thì dừng việc kiểm tra
         var rules = selectorRules[rule.selector];
         for (var i = 0; i < rules.length; i++) {
-            errorMessage = rules[i](inputElement.value);
+            switch (inputElement.type) {
+                case 'radio':
+                case 'checkbox':
+                    // Đoạn này chưa xong
+                    // errorMessage = rules[i](
+                    //     formElement.querySelector(rule.selector + ':checked')
+                    // );
+                    break;
+                default:
+                    errorMessage = rules[i](inputElement.value);
+            }
+
             if (errorMessage) break;
         }
 
         if (errorMessage) {
             errorElement.innerText = errorMessage;
-            inputElement.parentElement.classList.add('invalid');
+            getParent(inputElement, options.formGroupSelector).classList.add('invalid');
         } else {
             errorElement.innerText = '';
-            inputElement.parentElement.classList.remove('invalid');
+            getParent(inputElement, options.formGroupSelector).classList.remove('invalid');
         }
 
         return !errorMessage;
     }
+
+    var formElement = document.querySelector(options.form);
 
     if (formElement) {
 
@@ -51,7 +71,8 @@ function Validator(options) {
                 if (typeof options.onSubmit === 'function') {
                     var enableInputs = formElement.querySelectorAll('[name]:not([disabled])');
                     var formValues = Array.from(enableInputs).reduce(function (values, input) {
-                        return (values[input.name] = input.value) && values;
+                        values[input.name] = input.value;
+                        return values;
                     }, {});
                     options.onSubmit(formValues)
                 }
@@ -83,10 +104,10 @@ function Validator(options) {
 
                 // Xử lý trường hợp mỗi khi người dùng nhập vào input
                 inputElement.oninput = function () {
-                    // console.log(inputElement.parentElement.querySelector('.form-message')); Lấy thẻ span từ thẻ cha form-group
-                    var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
+                    // console.log(getParent(inputElement, options.formGroupSelector).querySelector('.form-message')); Lấy thẻ span từ thẻ cha form-group
+                    var errorElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector);
                     errorElement.innerText = '';
-                    inputElement.parentElement.classList.remove('invalid');
+                    getParent(inputElement, options.formGroupSelector).classList.remove('invalid');
                 }
             }
         });
@@ -134,4 +155,6 @@ Validator.isConfirmed = function (selector, getConfirmValue, message) {
         }
     }
 }
+
+// Học callback function
 
