3e8d366eb539b82143cfbf80baf24390f7a83748
HoangTuanAnh26 <tuanhoanga789@gmail.com>
2025-03-02 15:26:53 +0900

    Cách làm 2 code đến phút 34

---
+++ JavaScript/Form validation - Cach lam 2/validator.js
@@ -0,0 +1,64 @@
+function Validator(formSelecter) {
+    var formRules = {};
+
+    var validatorRules = {
+        required: function (value) {
+            return value ? undefined : 'Vui lòng nhập trường này';
+        },
+        email: function (value) {
+            var regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+            return regex.test(value) ? undefined : 'Vui lòng nhập email';
+        },
+        min: function (min) {
+            return function (value) {
+                return value.length >= min ? undefined : `Vui lòng nhập ít nhất ${min} ký tự`;
+            }
+        },
+        max: function (max) {
+            return function (value) {
+                return value.length <= max ? undefined : `Vui lòng nhập nhiều nhất ${max} ký tự`;
+            }
+        }
+    };
+
+    var formElemnt = document.querySelector(formSelecter);
+
+    if (formElemnt) {
+        var inputs = formElemnt.querySelectorAll('[name][rules]');
+
+        for (var input of inputs) {
+
+            var rules = input.getAttribute('rules').split('|');
+            for (var rule of rules) {
+                var ruleInfo;
+                var isRuleHasValue = rule.includes(':');
+
+                if (isRuleHasValue) {
+                    ruleInfo = rule.split(':');
+                    rule = ruleInfo[0];
+                }
+                var ruleFunc = validatorRules[rule];
+
+                if (isRuleHasValue) {
+                    ruleFunc = ruleFunc(ruleInfo[1]);
+                }
+
+                if (Array.isArray(formRules[input.name])) {
+                    formRules[input.name].push(ruleFunc);
+                } else {
+                    formRules[input.name] = [ruleFunc];
+                }
+            }
+            input.onblur = handleValidate;
+        }
+        function handleValidate(event) {
+            console.log(formRules[event.target.name]);
+        }
+    }
+    console.log(formRules);
+}
+
+
+
+
+
